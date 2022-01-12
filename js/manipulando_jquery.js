7ef9ce7d161c9da1d32de0cf0433932a9241deaa
Vinicius Sales <41462020+Vini-Salles@users.noreply.github.com>
2022-01-12 01:23:34 -0300

    Commit inicial
    Primeiro Commit do site

---
+++ js/manipulando_jquery.js
@@ -0,0 +1,18 @@
+$("#traco-menu").on('mouseover', function() {
+    $("#nav-menu").attr('style', 'width: 300px!important');
+    $("#list-group-flush").attr('style', 'display: block');
+    $("#footer").attr('style', 'margin-left: 300px!important');
+    $("#coluna1").attr('style', 'margin-left: 300px!important');
+});
+
+$("#btn-sanduiche").click(function() {
+    $("#nav-menu").attr('style', 'width: 71px!important');
+    $("#list-group-flush").attr('style', 'display: none');
+    $("#footer").attr('style', 'margin-left: 72px!important');
+    $("#coluna1").attr('style', 'margin-left: 70px!important');
+});
+
+$("#vendas").click(function() {
+    $(location).attr('href', 'tela_caixa.html');
+});
+
